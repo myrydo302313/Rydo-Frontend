f74e789b65fff1eb00412639971b0eee2c2dc7e7
Rydo <myrydo@gmail.com>
2025-02-26 00:16:43 +0530

    User Profile Section Updated

--- src/pages/UserAccount.jsx
+++ src/pages/UserAccount.jsx
@@ -1,22 +1,57 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../store/auth";
 import BottomNav from "../components/BottomNav";
+import "../styles/UserAccount.css";
+import { useNavigate } from "react-router-dom";
 
 const UserAccount = () => {
+  const [currUser, setCurrUser] = useState({});
 
-    const [currUser,setCurrUser]=useState({})
+  const { user, logoutUser } = useAuth();
 
-  const { user,logoutUser } = useAuth();
+  const navigate = useNavigate();
 
-  useEffect(()=>{
-    setCurrUser(user?.userData)
-  })
+  useEffect(() => {
+    setCurrUser(user?.userData);
+  });
 
+  const handleLogout = () => {
+    logoutUser();
+    navigate("/login");
+  };
 
   return (
     <>
-        <h1>Welcome {currUser?.name}</h1>
-        <button style={{backgroundColor:'#e71d36',padding:'7px',color:'white',width:'100px'}} onClick={logoutUser}>Logout</button>
+      <div className="profile-main">
+        <div className="user-name">{currUser?.name}</div>
+        <div className="user-phone">{currUser?.phone}</div>
+      </div>
+
+      <div className="profile-menu">
+        <div className="profile-menu-option">
+          <img src="/images/help.png" alt="Help" width={25} height={25} />
+          <p>Help</p>
+        </div>
+        <div className="profile-menu-option">
+          <img src="/images/payment.png" alt="Help" width={25} height={25} />
+          <p>Payment</p>
+        </div>
+        <div className="profile-menu-option">
+          <img src="/images/rides.png" alt="Help" width={25} height={25} />
+          <p>My Rides</p>
+        </div>
+        <div className="profile-menu-option">
+          <img src="/images/rewards.png" alt="Help" width={25} height={25} />
+          <p>My Rewards</p>
+        </div>
+      </div>
+
+      <div className="logout-btn-div">
+        <button className="logout-btn" onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
+
       <BottomNav />
     </>
   );
