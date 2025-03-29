7811ff97729ec2c34a22cff93bf99aced68c3071
Rydo <myrydo@gmail.com>
2025-03-29 12:42:19 +0530

    Services Section Updated

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,26 +1,26 @@
 import React from "react";
-import '../styles/Services.css'
+import "../styles/Services.css";
 
 const Services = () => {
   return (
     <>
-        <h2 align='center'>Our Services</h2>
+      <h2 align="center">Our Services</h2>
       <div className="services">
         <div className="service">
           <img src="/images/auto.jpg" width={80} height={80} alt="" />
           <p>Auto</p>
         </div>
         <div className="service">
-          <img src="/images/cab.jpg" width={80} height={120} alt="" />
-          <p>Cab</p>
-        </div>
-        <div className="service">
-          <img src="/images/bike.jpg" width={80} height={80} alt="" />
+          <img src="/images/rydo_bike.webp" width={80} height={80} alt="" />
           <p>Moto</p>
         </div>
         <div className="service">
           <img src="/images/courier.jpg" width={80} height={80} alt="" />
           <p>Courier</p>
+        </div>
+        <div className="service">
+          <img src="/images/cab.jpg" width={80} height={120} alt="" />
+          <p>Cab</p>
         </div>
       </div>
     </>
--- src/pages/RydoServices.jsx
+++ src/pages/RydoServices.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
-import BottomNav from '../components/BottomNav'
-import '../styles/RydoServices.css'
+import React from "react";
+import BottomNav from "../components/BottomNav";
+import "../styles/RydoServices.css";
 
 const RydoServices = () => {
   return (
     <>
-        <h2 className='services-head' align='center'>Our Services</h2>
-        <div className="services">
-            <div className="service-element">
-                <img src="/images/autoBanner.jpg" alt=""/>
-                <span>Rydo Auto</span>
-            </div>
-            <div className="service-element">
-                <img src="/images/cabBanner.jpg" alt=""/>
-                <span>Rydo Cab</span>
-            </div>
-            <div className="service-element">
-                <img src="/images/parcelBanner2.jpg" alt=""/>
-                <span>Rydo Parcel</span>
-            </div>
+      <h2 className="services-head" align="center">
+        Our Services
+      </h2>
+      <div className="services">
+        <div className="service-element">
+          <img src="/images/autoBanner.jpg" alt="" />
+          <span>Rydo Auto</span>
         </div>
-        <BottomNav/>
+        <div className="service-element">
+          <img src="/images/rydo_bike.webp"alt="" />
+          <span>Rydo Moto</span>
+        </div>
+        <div className="service-element">
+          <img src="/images/cabBanner.jpg" alt="" />
+          <span>Rydo Cab</span>
+        </div>
+        <div className="service-element">
+          <img src="/images/parcelBanner2.jpg" alt="" />
+          <span>Rydo Parcel</span>
+        </div>
+      </div>
+      <BottomNav />
     </>
-  )
-}
+  );
+};
 
-export default RydoServices
+export default RydoServices;
